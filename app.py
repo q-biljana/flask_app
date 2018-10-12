1c2dcf92c259241e96713bb0125677d62094efbb
q-biljana <biljana@qordoba.com>
2018-10-11 22:23:12 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask import request
+
+app = Flask(__name__)
+
+
+
+@app.route('/restapi', methods=['POST'])
+def hello():
+	user_str = request.json.get("userstr")
+	if user_str == "Hello":
+		myresponse="Hi!"
+		return jsonify( myresponse )
+	else:
+		abort(404)#, {'message': 'Wrong input, Say "Hello"'})
+
+if __name__ == '__main__':
+    app.run()
